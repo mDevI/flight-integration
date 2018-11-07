215afcb3438c95bfa3cc72d42f60213bb9978e77
Sergei.Belonosov <2belonosov@gmail.com>
2018-11-07 20:46:55 +0700

    Initial commit.

---
+++ src/main/java/com/mdevi/flightintegration/FlightIntegrationApplication.java
@@ -0,0 +1,12 @@
+package com.mdevi.flightintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightIntegrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightIntegrationApplication.class, args);
+    }
+}
---
+++ src/test/java/com/mdevi/flightintegration/FlightIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mdevi.flightintegration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightIntegrationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
